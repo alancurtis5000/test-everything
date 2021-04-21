17a568f1a80a9a2d4ceea2f529c342c11c97858e
Alan <alancurtis5000+accounts@gmail.com>
2021-04-20 21:38:36 -0600

    added example api call to examples

--- src/pages/examples/examples.component.jsx
+++ src/pages/examples/examples.component.jsx
@@ -1,8 +1,10 @@
 import ExampleCounter from "../../components/example-counter/example-counter.component";
+import ExampleApiCall from "../../components/example-api-call/example-api-call.component";
 const ExamplesPage = () => {
   return (
     <div>
       Examples Page
+      <ExampleApiCall />
       <ExampleCounter />
     </div>
   );
