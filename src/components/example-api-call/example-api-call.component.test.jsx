0396ac3fe26bfae3b669ae09e714126c7df59ebf
Alan <alancurtis5000+accounts@gmail.com>
2021-04-20 21:26:18 -0600

    TDD with api call

---
+++ src/components/example-api-call/example-api-call.component.test.jsx
@@ -0,0 +1,21 @@
+describe("ExampleApiCall", () => {
+  it("should match snapshot", () => {
+    expect(false).toBe(true);
+  });
+
+  it("should request api call", () => {
+    expect(false).toBe(true);
+  });
+  it("should show loading spinner", () => {
+    expect(false).toBe(true);
+  });
+  it("should show error on api call error", () => {
+    expect(false).toBe(true);
+  });
+  it("should show no results found on empty return", () => {
+    expect(false).toBe(true);
+  });
+  it("should show correct results after api call", () => {
+    expect(false).toBe(true);
+  });
+});
