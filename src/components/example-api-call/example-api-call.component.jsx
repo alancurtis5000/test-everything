4f64fca3518d86781ce9f26ac11c875234199862
Alan <alancurtis5000+accounts@gmail.com>
2021-04-21 22:21:21 -0600

    added lodash and no results found

--- src/components/example-api-call/example-api-call.component.jsx
+++ src/components/example-api-call/example-api-call.component.jsx
@@ -1,5 +1,7 @@
 import { connect } from "react-redux";
 import { exampleApiCall } from "../../redux/example-api-call/example-api-call.actions";
+import isEmpty from "lodash/isEmpty";
+
 const ExampleApiCall = (props) => {
   const { exampleApiCall, exampleApiCallRedux } = props;
   const { error, data, isLoaded } = exampleApiCallRedux;
@@ -18,10 +20,21 @@ const ExampleApiCall = (props) => {
     return isLoaded ? null : <div>...Loading...</div>;
   };
 
+  const renderNoResults = () => {
+    let check = isEmpty(data);
+    console.log({ check });
+    if (isEmpty(data) && isLoaded) {
+      return <div>...No results found...</div>;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div className="example-api-call">
       <h1>ExampleApiCall</h1>
       {renderLoading()}
+      {renderNoResults()}
       <div>
         <div>Quote:</div>
         <div>{text}</div>
@@ -29,7 +42,6 @@ const ExampleApiCall = (props) => {
         <div>{author}</div>
       </div>
       {renderError()}
-
       <button onClick={handleOnClick}>Get Quote</button>
     </div>
   );
